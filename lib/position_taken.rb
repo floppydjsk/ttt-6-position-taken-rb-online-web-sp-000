bcb492c6f348f770ca3d18d13dd62d34178b710a
Daniel Kim <daniel.kim52@gmail.com>
2019-10-10 06:40:10 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,16 +1,6 @@
-# code your #position_taken? method here!
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+def position_taken?(board, index)
 
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
+  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+    false : true
 
-def move(board, index, current_player = "X")
-  board[index] = current_player
 end
